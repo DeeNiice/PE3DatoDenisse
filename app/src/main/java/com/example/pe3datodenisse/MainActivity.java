194e22eea49d800845669e055e8f582758d45787
AGirlisnoone <DenisseDato@users.noreply.github.com>
2019-12-07 12:20:51 +0800

    Initial Commit

--- app/src/main/java/com/example/pe3datodenisse/MainActivity.java
+++ app/src/main/java/com/example/pe3datodenisse/MainActivity.java
@@ -9,94 +9,114 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    EditText cn, vn, rd, al;
-    DBHelper helper;
+    EditText etName, etNumber, etDate, etLevel;
+    com.example.pe3datodenisse.DBHelper helper;
     Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        helper = new com.example.pe3datodenisse.DBHelper(this);
         cursor = helper.getTable();
-        helper = new DBHelper(this);
-        cn = findViewById(R.id.et1);
-        vn = findViewById(R.id.et2);
-        rd = findViewById(R.id.et3);
-        al = findViewById(R.id.et4);
-
+        etName = findViewById(R.id.et1);
+        etNumber = findViewById(R.id.et2);
+        etDate = findViewById(R.id.et3);
+        etLevel= findViewById(R.id.et4);
     }
 
     public void addRecord(View v){
-        String codeName = cn.getText().toString();
-        String verNo = vn.getText().toString();
-        String rDate = rd.getText().toString();
-        String api = al.getText().toString();
-        long isInserted =  helper.insert(codeName, verNo, rDate, api);
-        if(isInserted == -1){
-            Toast.makeText(this, "record not inserted", Toast.LENGTH_LONG);
+        String codeName         = etName.getText().toString();
+        String versionNumber    = "Ver. "+etNumber.getText().toString();
+        String releaseDate      = "Released "+etDate.getText().toString();
+        String APILevel         = "API Level"+etLevel.getText().toString();
+        long isInserted = helper.insert(codeName, versionNumber, releaseDate, APILevel);
+        if (isInserted == -1){
+            Toast.makeText(this, "Record not added", Toast.LENGTH_LONG).show();
         }else{
-            Toast.makeText(this,"record inserted", Toast.LENGTH_LONG);
+            Toast.makeText(this, "Record saved..", Toast.LENGTH_LONG).show();
         }
     }
 
     public void moveFirst(View v){
-
         cursor.moveToFirst();
-        cursor.getString();
-        String codeName = cursor.getString(1);
-        String verNo = cursor.getString(2);
-        String relDate = cursor.getString(3);
-        String apiLevel = cursor.getString(4);
-
-        cn.setText(codeName);
-        vn.setText(verNo);
-        rd.setText(relDate);
-        al.setText(apiLevel);
+        String codeName         = cursor.getString(1);
+        String versionNumber    = cursor.getString(2);
+        String releaseDate      = cursor.getString(3);
+        String APILevel         = cursor.getString(4);
+        etName.setText(codeName);
+        etNumber.setText(versionNumber);
+        etDate.setText(releaseDate);
+        etLevel.setText(APILevel);
     }
 
-    public void moveNext(View v){
-
+    public void moveNext(View v) {
         cursor.moveToNext();
-        cursor.getString();
         String codeName = cursor.getString(1);
-        String verNo = cursor.getString(2);
-        String relDate = cursor.getString(3);
-        String apiLevel = cursor.getString(4);
+        String versionNumber = cursor.getString(2);
+        String releaseDate = cursor.getString(3);
+        String APILevel = cursor.getString(4);
+        etName.setText(codeName);
+        etNumber.setText(versionNumber);
+        etDate.setText(releaseDate);
+        etLevel.setText(APILevel);
+    }
 
-        cn.setText(codeName);
-        vn.setText(verNo);
-        rd.setText(relDate);
-        al.setText(apiLevel);
+    public void moveBack (View v){
+        cursor.moveToPrevious();
+        String codeName = cursor.getString(1);
+        String versionNumber = cursor.getString(2);
+        String releaseDate = cursor.getString(3);
+        String APILevel = cursor.getString(4);
+        etName.setText(codeName);
+        etNumber.setText(versionNumber);
+        etDate.setText(releaseDate);
+        etLevel.setText(APILevel);
+    }
 
+    public void moveLast (View v){
+        cursor.moveToLast();
+        String codeName = cursor.getString(1);
+        String versionNumber = cursor.getString(2);
+        String releaseDate = cursor.getString(3);
+        String APILevel = cursor.getString(4);
+        etName.setText(codeName);
+        etNumber.setText(versionNumber);
+        etDate.setText(releaseDate);
+        etLevel.setText(APILevel);
+    }
+
+    public void editRecord(View v){
+        String codeName        = etName.getText().toString();
+        String versionNumber   = "Ver. "+etNumber.getText().toString();
+        String releaseDate     = "Released "+etDate.getText().toString();
+        String APILevel        = "API Level"+etLevel.getText().toString();
+        String id              = cursor.getString(0);
+        int numUpdated = helper.update(id, codeName, versionNumber, releaseDate, APILevel);
+        if (numUpdated == 1){
+            Toast.makeText(this, "Record updated..", Toast.LENGTH_LONG).show();
+        }else{
+            Toast.makeText(this, "Update failed..", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void removeRecord(View v){
         String id = cursor.getString(0);
-        int numDeleted = helper.delete(id);
-
-        if(numDeleted == 1){
-            Toast.makeText(this,"record removed", Toast.LENGTH_LONG);
+        int numDeleted =  helper.delete(id);
+        if (numDeleted == 1){
+            Toast.makeText(this, "Record removed..", Toast.LENGTH_LONG).show();
         }else{
-            Toast.makeText(this,"record not removed", Toast.LENGTH_LONG);
+            Toast.makeText(this, "Deletion failed..", Toast.LENGTH_LONG).show();
         }
     }
 
-    public void editRecord(View v){
-        String codeName = cn.getText().toString();
-        String verNo = vn.getText().toString();
-        String rDate = rd.getText().toString();
-        String api = al.getText().toString();
-        String id = cursor.getString(0);
-        int numUpdated = helper.update(id, codeName,verNo,rDate,api);
+    public void clearRecord(View v){
+        etName.setText("");
+        etNumber.setText("");
+        etDate.setText("");
+        etLevel.setText("");
 
-        if(numUpdated == 1){
-            Toast.makeText(this,"record updated", Toast.LENGTH_LONG);
-        } else{
-            Toast.makeText(this,"record not updated", Toast.LENGTH_LONG);
-        }
     }
-
 
 
 }
